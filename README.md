ec431a3c4e04a11affa2ceab4e9b1d4228d0567a
EternalHeart <hao131462@qq.com>
2024-06-30 19:58:09 +0800

    feat: dictionary finish

--- README.md
+++ README.md
@@ -1,16 +1,39 @@
-# liuyao_flutter
+# 六爻排盘
 
-A flutter project for liuyao.
+一个用于六爻排卦的APP。
 
-## Getting Started
+## 关于六爻
 
-This project is a starting point for a Flutter application.
+#### 原则:
+不诚不占，不义不占，不疑不占。
 
-A few resources to get you started if this is your first Flutter project:
+#### 排卦方法
+原料: 三枚硬币(一枚也可以)
+方式: 心中默念自己想要得到答案的问题，在手中摇动硬币并掷出硬币落地，三枚一组，记下硬币的正反面排布（一枚硬币的话就是掷三次为一组查看）对应的数字，进行六次投掷后成卦。
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+#### 硬币排布对照表
+| 对应数字 | 含义 | 符号记法      | 硬币排布情况 |
+|------|----|-----------|--------|
+| 6    | 老阴 | `--x`或`x` | 三字     |
+| 7    | 少阳 | `—`或`,`   | 两花一字   |
+| 8    | 少阴 | `--`或`,,` | 两字一花   |
+| 9    | 老阳 | `—o`或`o`  | 三花     |
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### 卦象对应的含义
+| 名称 | 含义                                |
+|----|-----------------------------------|
+| 本卦 | 直接得到的卦象，代表了占卜问题的当前状态或者初始条件。       |
+| 变卦 | 本卦中的动爻阴阳互变，代表事物发展方向和结局。           |
+| 错卦 | 将本卦的六个爻全部变为相反的爻，代表转机。             |
+| 综卦 | 将本卦的六个爻整个翻转形成的卦，代表从事物的反面来看待事物的发展。 |
+| 互卦 | 将本卦的三四五爻作为上卦，二三四爻变为下卦，代表事物的发展过程。  |
+
+## 口诀
+☰ 乾三连，
+☷ 坤六断。
+☳ 震仰盂，
+☶ 艮覆碗。
+☲ 离中虚，
+☵ 坎中满。
+☱ 兑上缺，
+☴ 巽下断。
--- lib/constants/xiang.dictionary.dart
+++ lib/constants/xiang.dictionary.dart
@@ -1,30 +1,112 @@
-import 'package:liuyao_flutter/constants/xiangs/qian_wei_tian.dart';
+import 'package:liuyao_flutter/constants/xiangs/index.dart';
 
-class XiangDicItem{
+class XiangDicItem {
   // 象名称
   String name;
+
   // 象全名
   String fullName;
+
   // 象简述
   String simpleDescription;
+
   // 象的本卦原文
   String originalHexagram;
+
   // 象的初爻
   String initialLine;
+
   // 象的二爻
   String secondLine;
+
   // 象的三爻
   String thirdLine;
+
   // 象的四爻
   String fourthLine;
+
   // 象的五爻
   String fifthLine;
+
   // 象的上爻
   String uppermostLine;
 
-  XiangDicItem(this.name,this.fullName,this.simpleDescription,this.originalHexagram,this.initialLine,this.secondLine,this.thirdLine,this.fourthLine,this.fifthLine,this.uppermostLine);
+  XiangDicItem(this.name, this.fullName, this.simpleDescription,
+      this.originalHexagram, this.initialLine, this.secondLine, this.thirdLine,
+      this.fourthLine, this.fifthLine, this.uppermostLine);
 }
+
 /// 象表映射字典 象名为键名 象为值
-Map<String,XiangDicItem> xiangDictionary = {
-  "乾":qianWeiTian,
+Map<String, XiangDicItem> xiangDictionary = {
+  "乾": qianWeiTian,
+  "履": tianZeLv,
+  "同人": tianHuoTongRen,
+  "无妄": tianLeiWuWang,
+  "姤": tianFengGou,
+  "讼": tianShuiSong,
+  "遁": tianShanDun,
+  "否": tianDiPi,
+  "夬": zeTianGuai,
+  "兑": duiWeiZe,
+  "革": zeHuoGe,
+  "随": zeLeiSui,
+  "大过": zeFengDaGuo,
+  "困": zeShuiKun,
+  "咸": zeShanXian,
+  "萃": zeDiCui,
+  "大有": tianHuoDaYou,
+  "睽": huoZeKui,
+  "离": liWeiHuo,
+  "噬嗑":huoLeiSHiKe,
+  "鼎": huoFengDing,
+  "未济": huoShuiWeiJi,
+  "旅": huoShanLv,
+  "晋": huoDiJin,
+  "大壮": leiTianDaZhuang,
+  "归妹": leiZeGuiMei,
+  "丰": leiHuoFeng,
+  "震": zhenWeiLei,
+  "恒": leiFengHeng,
+  "解": leiShuiJie,
+  "小过": leiShanXiaoGuo,
+  "豫": leiDiYu,
+  "小畜": fengTianXiaoChu,
+  "中孚": fengZeZhongFu,
+  "家人": fengHuoJiaRen,
+  "益": fengLeiYi,
+  "巽": xunWeiFeng,
+  "涣": fengShuiHuan,
+  "渐": fengShanJian,
+  "观": fengDiGuan,
+  "需": shuiTianXu,
+  "节": shuiZeJie,
+  "既济": shuiHuoJiJi,
+  "屯": shuiLeiTun,
+  "井": shuiFengJing,
+  "坎": kanWeiShui,
+  "蹇": shuiShanJian,
+  "比": shuiDiBi,
+  "大畜": shanTianDaChu,
+  "损": shanZeSun,
+  "贲": shanHuoBen,
+  "颐": shanLeiYi,
+  "蛊": shanFengGu,
+  "蒙": shanShuiMeng,
+  "艮": genWeiShan,
+  "剥": shanDiBo,
+  "泰": diTianTai,
+  "临": diZeLin,
+  "明夷": diHuoMingYi,
+  "复": diLeiFu,
+  "升": diFengSheng,
+  "师": diShuiShi,
+  "谦": diShanQian,
+  "坤": kunWeiDi
 };
+// 网站勘误
+// https://www.zhouyi.cc/zhouyi/yijing64/4257.html 原文
+// https://www.zhouyi.cc/zhouyi/yijing64/4188.html 六四爻 结尾
+// https://www.zhouyi.cc/zhouyi/yijing64/4179.html 九二
+// https://www.zhouyi.cc/zhouyi/yijing64/4111.html 九初
+// https://www.zhouyi.cc/zhouyi/yijing64/4159.html
+// https://www.zhouyi.cc/zhouyi/yijing64/4126.html 泰卦变豫卦
