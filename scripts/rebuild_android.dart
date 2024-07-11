5865d1f56b0a6550365fe0de62b95bd5deb14670
EternalHeart <hao131462@qq.com>
2024-07-11 10:48:51 +0800

    chore: better way to run command

--- scripts/rebuild_android.dart
+++ scripts/rebuild_android.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:path/path.dart' as path;
 
 import 'utils.dart';
@@ -6,6 +8,7 @@ void main() async {
   // 获取脚本所在目录的路径
   final rootPath = ScriptUtil.rootPath();
   final androidGradleW = path.join(rootPath, 'android');
-  ScriptUtil.runScript('gradlew.bat clean', androidGradleW, null, null);
-  ScriptUtil.runScript('gradlew.bat build', androidGradleW, null, null);
+  final gradlew = Platform.isWindows?'./gradlew.bat':'./gradlew';
+  ScriptUtil.runScript('$gradlew clean', androidGradleW, null, null);
+  ScriptUtil.runScript('$gradlew build', androidGradleW, null, null);
 }
--- scripts/utils.dart
+++ scripts/utils.dart
@@ -13,49 +13,54 @@ class ScriptUtil {
   /// 执行脚本
   static void runScript(String command, String workspaceDir,
       CallbackFunction? success, CallbackFunction? fail) async {
-    StreamController<String> streamController = StreamController();
-    // 执行传入的命令
-    print("[RUN]: $command");
-    var process = await Process.start(
-      command,
-      [],
-      runInShell: true,
-      workingDirectory: workspaceDir,
-    );
-    // 将进程的标准输出连接到流控制器
-    process.stdout.transform(utf8.decoder).listen((String output) {
-      // 实时打印输出
-      streamController.add(output);
-    });
-    process.stderr.transform(utf8.decoder).listen((String output) {
-      // 实时打印输出
-      streamController.add(output);
-    });
-    // 监听流，实时输出数据
-    streamController.stream.listen((String output) {
-      print(output);
-    });
-    process.exitCode.then((int code) {
-      if (code == 0) {
+    final streamController = StreamController<String>();
+    try {
+      // 确定要使用的shell命令
+      String shell;
+      List<String> arguments;
+      if (Platform.isWindows) {
+        shell = 'cmd';
+        arguments = ['/c', command];
+      } else {
+        shell = 'bash';
+        arguments = ['-c', command];
+      }
+
+      // 执行传入的命令
+      print("[RUN]: $command");
+      final process = await Process.start(
+        shell,
+        arguments,
+        runInShell: true,
+        workingDirectory: workspaceDir,
+      );
+
+      // 将进程的标准输出连接到流控制器
+      process.stdout.transform(utf8.decoder).listen(streamController.add);
+      process.stderr.transform(utf8.decoder).listen(streamController.add);
+
+      // 监听流，实时输出数据
+      streamController.stream.listen(print);
+
+      // 等待进程退出
+      final exitCode = await process.exitCode;
+      if (exitCode == 0) {
         // 如果命令成功执行，调用成功回调函数
-        if (success != null) {
-          success();
-        }
+        success?.call();
       } else {
         // 打印错误信息
-        print('[RUN:error]: Script execution failed');
+        print('[RUN:error]: Script execution failed with exit code $exitCode');
         // 调用失败回调函数
-        if (fail != null) {
-          fail();
-        }
+        fail?.call();
       }
-    }, onError: (err) {
+    } catch (err) {
       // 捕获并打印异常信息
       print('[RUN:error]: Exception occurred: $err');
       // 调用失败回调函数
-      if (fail != null) {
-        fail();
-      }
-    });
+      fail?.call();
+    } finally {
+      // 关闭流控制器
+      await streamController.close();
+    }
   }
 }
