19ca1af8f351f16ccf07e32a043bdf9da62164ff
EternalHeart <hao131462@qq.com>
2024-09-13 00:48:07 +0800

    feat: Xiao Liu Ren ver1

--- lib/constants/liuren.const.dart
+++ lib/constants/liuren.const.dart
@@ -2,15 +2,15 @@ import 'package:liuyao/constants/common.const.dart';
 
 /// 小六壬宫位
 enum XiaoLiuRen {
-  daan("大安", "长期 缓慢 稳定", "木", TraditionalDirection.east,"三清", 1),
-  liulian("留连","停止 反复 复杂","木",TraditionalDirection.southwest,"文昌",2),
-  suxi("速喜","惊喜 快速 突然","火",TraditionalDirection.south,"雷祖",3),
-  chikou("赤口","争斗 凶恶 伤害","金",TraditionalDirection.west,"将帅",4),
-  xiaoji("小吉","起步 不多 尚可","水",TraditionalDirection.north,"真武",5),
-  kongwang("空亡","失去 虚伪 空想","土",TraditionalDirection.middle,"玉皇",6),
-  bingfu("病符","病态 异常 治疗","金",TraditionalDirection.southwest,"后土",7),
-  taohua("桃花","欲望 牵绊 异性","土",TraditionalDirection.northeast,"城隍",8),
-  tiande("天德","贵人 上司 高选","金",TraditionalDirection.northwest,"紫薇",9);
+  daan("大安", "长期 缓慢 稳定", "木", TraditionalDirection.east, "三清", 1),
+  liulian("留连", "停止 反复 复杂", "木", TraditionalDirection.southwest, "文昌", 2),
+  suxi("速喜", "惊喜 快速 突然", "火", TraditionalDirection.south, "雷祖", 3),
+  chikou("赤口", "争斗 凶恶 伤害", "金", TraditionalDirection.west, "将帅", 4),
+  xiaoji("小吉", "起步 不多 尚可", "水", TraditionalDirection.north, "真武", 5),
+  kongwang("空亡", "失去 虚伪 空想", "土", TraditionalDirection.middle, "玉皇", 6),
+  bingfu("病符", "病态 异常 治疗", "金", TraditionalDirection.southwest, "后土", 7),
+  taohua("桃花", "欲望 牵绊 异性", "土", TraditionalDirection.northeast, "城隍", 8),
+  tiande("天德", "贵人 上司 高选", "金", TraditionalDirection.northwest, "紫薇", 9);
 
   final String name;
   final String description;
@@ -18,12 +18,32 @@ enum XiaoLiuRen {
   final TraditionalDirection direction;
   final String shen;
   final int order;
-  const XiaoLiuRen(this.name, this.description, this.property, this.direction,this.shen,this.order);
+
+  const XiaoLiuRen(this.name, this.description, this.property, this.direction,
+      this.shen, this.order);
+
+  /// 获取第一个
+  static XiaoLiuRen getFirst() {
+    return XiaoLiuRen.daan;
+  }
 
   /// 获取按序小六壬
-  static List<XiaoLiuRen> getXiaoLiuRenByOrder(){
+  static List<XiaoLiuRen> getXiaoLiuRenListByOrder() {
     var list = List<XiaoLiuRen>.from(XiaoLiuRen.values);
     list.sort((a, b) => a.order - b.order);
     return list;
+  }
+
+  /// 通过index获取
+  static XiaoLiuRen getXiaoLiuRenByOrder(int order) {
+    return XiaoLiuRen.values.where((e) => e.order == order).first;
+  }
+
+  /// 从起始六壬走步数
+  static XiaoLiuRen getXiaoLiuRenByStep(XiaoLiuRen from, int step) {
+    if (step == 0) return from;
+    int first = ((from.order - 1) + (step % 10)) % 10;
+    int target = first == 0 ? 1 : first;
+    return getXiaoLiuRenByOrder(target);
   }
 }
--- lib/pages/xiaoliuren/xiaoliuren.dart
+++ lib/pages/xiaoliuren/xiaoliuren.dart
@@ -1,8 +1,10 @@
-import 'dart:math';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:liuyao/constants/liuren.const.dart';
-import 'package:liuyao/utils/logger.dart';
+import 'package:lunar/lunar.dart';
+
+import '../../utils/logger.dart';
 
 class XiaoLiuRenPage extends StatefulWidget {
   @override
@@ -12,43 +14,297 @@ class XiaoLiuRenPage extends StatefulWidget {
 }
 
 class _XiaoLiuRen extends State<StatefulWidget> {
+  // 用于输入的三个数字
+  final TextEditingController _num1Controller = TextEditingController();
+  final TextEditingController _num2Controller = TextEditingController();
+  final TextEditingController _num3Controller = TextEditingController();
+
+  // 控制是否显示错误提示
+  bool _isNum1Valid = true;
+  bool _isNum2Valid = true;
+  bool _isNum3Valid = true;
+  String lunarInfo = "";
+
+  // 记录选中的宫位索引，用于高亮
+  List<XiaoLiuRen> _selectedXiaoLiuRen = [];
+  Timer? _timer;
+
   @override
   void initState() {
-    logger.info(XiaoLiuRen.getXiaoLiuRenByOrder());
+    super.initState();
+    setLunarInfo();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setLunarInfo();
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  void setLunarInfo() {
+    setState(() {
+      Lunar lunar = Lunar.fromDate(DateTime.now());
+      Lunar nextDayLunar =
+          Lunar.fromDate(DateTime.now().add(Duration(seconds: 24 * 60 * 60)));
+      lunarInfo =
+          "${lunar.getYearInGanZhi()}年 ${lunar.getMonthInGanZhi()}月 ${lunar.getHour() == 23 ? nextDayLunar.getDayInGanZhi() : lunar.getDayInGanZhi()}日 ${lunar.getTimeInGanZhi()}时 (${lunar.getHour().toString().padLeft(2, '0')}:${lunar.getMinute().toString().padLeft(2, '0')}:${lunar.getSecond().toString().padLeft(2, '0')})";
+    });
+  }
+  // 获取农历时间数字
+  List<int> getNumByLunar(){
+    Lunar lunar = Lunar.fromDate(DateTime.now());
+    Lunar nextDayLunar =
+    Lunar.fromDate(DateTime.now().add(Duration(seconds: 24 * 60 * 60)));
+    return [LunarUtil.MONTH.indexOf(lunar.getMonthInChinese()) ,lunar.getHour()!=23? LunarUtil.DAY.indexOf(lunar.getDayInChinese()) :LunarUtil.DAY.indexOf(nextDayLunar.getDayInChinese()),lunar.getTimeZhiIndex()+1];
+  }
+
+  void _onTime(){
+    List<int> numList = getNumByLunar();
+    setState(() {
+      _num1Controller.text = "${numList[0]}";
+      _num2Controller.text = "${numList[1]}";
+      _num3Controller.text = "${numList[2]}";
+    });
+  }
+  // 排盘按钮点击事件
+  void _onPaiPan() {
+    _isNum1Valid = _num1Controller.text.isNotEmpty;
+    _isNum2Valid = _num2Controller.text.isNotEmpty;
+    _isNum3Valid = _num3Controller.text.isNotEmpty;
+    if (_isNum1Valid && _isNum2Valid && _isNum3Valid) {
+      var numList = [
+        int.parse(_num1Controller.text),
+        int.parse(_num2Controller.text),
+        int.parse(_num3Controller.text)
+      ];
+      var cur = XiaoLiuRen.getFirst();
+      List<XiaoLiuRen> list = [];
+      for (int step in numList) {
+        cur = XiaoLiuRen.getXiaoLiuRenByStep(cur, step);
+        logger.info("$cur $step");
+        list.add(cur);
+      }
+      setState(() {
+        _selectedXiaoLiuRen = list;
+        logger.debug(list);
+      });
+    }
+  }
+
+  void _onReset() {
+    setState(() {
+      _num1Controller.clear();
+      _num2Controller.clear();
+      _num3Controller.clear();
+      _isNum1Valid = true;
+      _isNum2Valid = true;
+      _isNum3Valid = true;
+      _selectedXiaoLiuRen = [];
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('小六壬'),
+        appBar: AppBar(
+          title: Text(
+            '小六壬',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
-          body: GridView.count(
-            crossAxisCount: 3, // 每行3个宫位
+          backgroundColor: Colors.teal, // 修改AppBar背景颜色
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
             children: [
-              _buildGridItem('留连'),
-              _buildGridItem('速喜'),
-              _buildGridItem('病符'),
-              _buildGridItem('大安'),
-              _buildGridItem('空亡'),
-              _buildGridItem('赤口'),
-              _buildGridItem('桃花'),
-              _buildGridItem('小吉'),
-              _buildGridItem('天德'),
+              // 干支信息展示
+              TextButton(
+                onPressed: _onTime,
+                child: Text(
+                  lunarInfo,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              // 输入框和按钮
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      _buildNumberInput(_num1Controller, _isNum1Valid),
+                      if (_selectedXiaoLiuRen.length >= 3)
+                        Text(_selectedXiaoLiuRen.first.name)
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      _buildNumberInput(_num2Controller, _isNum2Valid),
+                      if (_selectedXiaoLiuRen.length >= 3)
+                        Text(_selectedXiaoLiuRen[1].name)
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      _buildNumberInput(_num3Controller, _isNum3Valid),
+                      if (_selectedXiaoLiuRen.length >= 3)
+                        Text(_selectedXiaoLiuRen.last.name)
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: _onPaiPan,
+                        child: Text("排盘"),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.teal, // 按钮颜色
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: _onReset,
+                        child: Text("重置"),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.teal, // 按钮颜色
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+
+              // 九宫格展示
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 3, // 每行3个宫位
+                  crossAxisSpacing: 10.0, // 网格项之间的水平间距
+                  mainAxisSpacing: 10.0, // 网格项之间的垂直间距
+                  children: _buildGongweiItems(),
+                ),
+              ),
             ],
           ),
+        ),
       ),
     );
   }
-  // 构建九宫格的每个Item
-  Widget _buildGridItem(String title) {
+
+  // 构建数字输入框
+  Widget _buildNumberInput(TextEditingController controller, bool isValid) {
+    return SizedBox(
+      width: 50,
+      child: TextField(
+        controller: controller,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: '数字',
+          errorText: isValid ? null : '卦数',
+        ),
+      ),
+    );
+  }
+
+  // 构建九宫格中的宫位项
+  List<Widget> _buildGongweiItems() {
+    List<XiaoLiuRen> allGongwei = [
+      XiaoLiuRen.liulian,
+      XiaoLiuRen.suxi,
+      XiaoLiuRen.bingfu,
+      XiaoLiuRen.daan,
+      XiaoLiuRen.kongwang,
+      XiaoLiuRen.chikou,
+      XiaoLiuRen.taohua,
+      XiaoLiuRen.xiaoji,
+      XiaoLiuRen.tiande,
+    ];
+
+    return List.generate(allGongwei.length, (index) {
+      if (_selectedXiaoLiuRen.length >= 3) {
+        bool isSelected = _selectedXiaoLiuRen.contains(allGongwei[index]);
+        int selectionIndex = _selectedXiaoLiuRen.indexOf(allGongwei[index]) + 1;
+        return _buildGridItem(allGongwei[index], isSelected, selectionIndex);
+      } else {
+        return _buildGridItem(allGongwei[index], false, 0);
+      }
+    });
+  }
+
+  // 构建每个宫位的卡片展示，增加高亮效果
+  Widget _buildGridItem(XiaoLiuRen xiang, bool isSelected, int selectionIndex) {
     return Card(
-      margin: EdgeInsets.all(8.0),
-      child: Center(
-        child: Text(
-          title,
-          style: TextStyle(fontSize: 20),
+      elevation: isSelected ? 10.0 : 4.0, // 选中的卡片增加阴影
+      color: isSelected ? Colors.teal[100] : Colors.white, // 选中时背景颜色变化
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0), // 设置圆角
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, // 垂直居中对齐
+                children: [
+                  Text(
+                    xiang.name,
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: isSelected ? Colors.teal : Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 4.0), // 控制不同文本之间的间距
+                  Text(
+                    xiang.direction.gua?.name ?? '',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                  SizedBox(height: 4.0),
+                  Text(
+                    xiang.description,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey[600],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            // 选中的宫位在右上角显示序号
+            if (isSelected)
+              Positioned(
+                right: -4,
+                top: -4,
+                child: CircleAvatar(
+                  radius: 12,
+                  backgroundColor: Colors.teal,
+                  child: Text(
+                    '$selectionIndex',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ),
+          ],
         ),
       ),
     );
