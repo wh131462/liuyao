be3030db398008902094763adc4af516136c1b4b
EternalHeart <hao131462@qq.com>
2024-10-08 21:54:12 +0800

    feat: read logic

--- lib/pages/book/book_list.dart
+++ lib/pages/book/book_list.dart
@@ -1,10 +1,5 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
-import 'package:path_provider/path_provider.dart';
 import '../../constants/book.list.dart';
 import 'epub_screen.dart';
 import 'pdf_screen.dart';
@@ -17,32 +12,6 @@ class BookList extends StatefulWidget {
 }
 
 class _BookList extends State<BookList> {
-  final Completer<PDFViewController> _controller =
-  Completer<PDFViewController>();
-  int? pages = 0;
-  int? currentPage = 0;
-  bool isReady = false;
-  String errorMessage = '';
-  Map<int, File> fileMap = {};
-
-  Future<File> fromAsset(String asset, String filename) async {
-    // To open from assets, you can copy them to the app storage folder, and the access them "locally"
-    Completer<File> completer = Completer();
-
-    try {
-      var dir = await getApplicationDocumentsDirectory();
-      File file = File("${dir.path}/$filename");
-      var data = await rootBundle.load(asset);
-      var bytes = data.buffer.asUint8List();
-      await file.writeAsBytes(bytes, flush: true);
-      completer.complete(file);
-    } catch (e) {
-      throw Exception('Error parsing asset file!');
-    }
-
-    return completer.future;
-  }
-
   @override
   void initState() {}
 
@@ -73,24 +42,18 @@ class _BookList extends State<BookList> {
             ),
             itemCount: bookList.length,
             itemBuilder: (context, index) {
-              fromAsset(bookList[index].path, bookList[index].name)
-                  .then((file) {
-                setState(() {
-                  fileMap[index] = file;
-                });
-              });
               return TextButton(
                 child: Text(bookList[index].name),
                 onPressed: () {
-                  if (fileMap[index]!.path.isNotEmpty) {
+                  if (bookList[index].path.isNotEmpty) {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
                         builder: (context) =>
                         bookList[index].path.endsWith("pdf") ? PDFScreen(
-                            path: fileMap[index]!.path,
+                            path: bookList[index].path,
                             name: bookList[index].name) :
-                        EPUBScreen(path: fileMap[index]!.path,
+                        EPUBScreen(path: bookList[index].path,
                             name: bookList[index].name),
                       ),
                     );
--- lib/pages/book/pdf_screen.dart
+++ lib/pages/book/pdf_screen.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
-
-import '../../utils/logger.dart';
+import 'package:path_provider/path_provider.dart';
 
 class PDFScreen extends StatefulWidget {
   final String? path;
@@ -16,12 +17,49 @@ class PDFScreen extends StatefulWidget {
 
 class _PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
   final Completer<PDFViewController> _controller =
-  Completer<PDFViewController>();
+      Completer<PDFViewController>();
+  late String currentPath;
   int? pages = 0;
   int? currentPage = 0;
   bool isReady = false;
   String errorMessage = '';
   bool _swipeHorizontal = true;
+
+  Future<File> fromAsset(String asset, String filename) async {
+    // To open from assets, you can copy them to the app storage folder, and the access them "locally"
+    Completer<File> completer = Completer();
+    try {
+      var dir = await getApplicationDocumentsDirectory();
+      File file = File("${dir.path}/$filename");
+      var data = await rootBundle.load(asset);
+      var bytes = data.buffer.asUint8List();
+      await file.writeAsBytes(bytes, flush: true);
+      completer.complete(file);
+    } catch (e) {
+      throw Exception('Error parsing asset file!');
+    }
+
+    return completer.future;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadPdf();
+  }
+
+  Future<void> loadPdf() async {
+    try {
+      File file = await fromAsset(widget.path!, widget.name!);
+      setState(() {
+        currentPath = file.path;
+      });
+    } catch (e) {
+      setState(() {
+        errorMessage = e.toString();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,79 +72,64 @@ class _PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
             onPressed: () {
               setState(() {
                 _swipeHorizontal = !_swipeHorizontal;
-                logger.debug(_swipeHorizontal);
               });
             },
           ),
         ],
       ),
-      body: Stack(
-        children: <Widget>[
-          PDFView(
-            filePath: widget.path,
-            enableSwipe: true,
-            swipeHorizontal: true,
-            autoSpacing: false,
-            pageFling: true,
-            pageSnap: true,
-            defaultPage: currentPage!,
-            fitPolicy: FitPolicy.BOTH,
-            preventLinkNavigation: false,
-            // if set to true the link is handled in flutter
-            onRender: (_pages) {
-              setState(() {
-                pages = _pages;
-                isReady = true;
-              });
-            },
-            onError: (error) {
-              setState(() {
-                errorMessage = error.toString();
-              });
-              print(error.toString());
-            },
-            onPageError: (page, error) {
-              setState(() {
-                errorMessage = '$page: ${error.toString()}';
-              });
-              print('$page: ${error.toString()}');
-            },
-            onViewCreated: (PDFViewController pdfViewController) {
-              _controller.complete(pdfViewController);
-            },
-            onLinkHandler: (String? uri) {
-              print('goto uri: $uri');
-            },
-            onPageChanged: (int? page, int? total) {
-              print('page change: $page/$total');
-              setState(() {
-                currentPage = page;
-              });
-            },
-          ),
-          errorMessage.isEmpty
-              ? !isReady
-              ? Center(
-            child: CircularProgressIndicator(),
-          )
-              : Container()
-              : Center(
-            child: Text(errorMessage),
-          )
-        ],
-      ),
+      body: currentPath.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : errorMessage.isNotEmpty
+              ? Center(child: Text(errorMessage))
+              : PDFView(
+                  filePath: currentPath,
+                  enableSwipe: true,
+                  swipeHorizontal: true,
+                  autoSpacing: false,
+                  pageFling: true,
+                  pageSnap: true,
+                  defaultPage: currentPage!,
+                  fitPolicy: FitPolicy.BOTH,
+                  preventLinkNavigation: false,
+                  onRender: (_pages) {
+                    setState(() {
+                      pages = _pages;
+                      isReady = true;
+                    });
+                  },
+                  onError: (error) {
+                    setState(() {
+                      errorMessage = error.toString();
+                    });
+                  },
+                  onPageError: (page, error) {
+                    setState(() {
+                      errorMessage = '$page: ${error.toString()}';
+                    });
+                  },
+                  onViewCreated: (PDFViewController pdfViewController) {
+                    _controller.complete(pdfViewController);
+                  },
+                  onLinkHandler: (String? uri) {
+                    print('goto uri: $uri');
+                  },
+                  onPageChanged: (int? page, int? total) {
+                    setState(() {
+                      currentPage = page;
+                    });
+                  },
+                ),
       floatingActionButton: FutureBuilder<PDFViewController>(
         future: _controller.future,
         builder: (context, AsyncSnapshot<PDFViewController> snapshot) {
           if (snapshot.hasData) {
             return FloatingActionButton.extended(
-              label: Text("Go to ${pages! ~/ 2}"),
+              label: Text("到${pages! ~/ 2}页"),
               onPressed: () async {
                 await snapshot.data!.setPage(pages! ~/ 2);
               },
             );
           }
-
           return Container();
         },
       ),
